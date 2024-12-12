b3fd9d6c4d087cd38d0651b7f16c81fe4772a00f
Sven Giesbrecht <sven.giesbrecht@posteo.de>
2024-12-12 16:23:35 +0100

    mart weather weekly

--- models/mart/mart_weather_weekly.sql
+++ models/mart/mart_weather_weekly.sql
@@ -5,10 +5,10 @@ SELECT
 	,min(min_temp_c) AS min_temp_w
 	,max(max_temp_c) AS max_temp_w
 	,round(avg(precipitation_mm),2) AS avg_precipitation_w
-	,avg(max_snow_mm) AS avg_snow_w
+	,max(max_snow_mm) AS avg_snow_w
 	,round(avg(avg_wind_direction),2) AS avg_wind_direction_w
 	,round(avg(avg_wind_speed_kmh),2) AS avg_wind_speed_w
-	,avg(wind_peakgust_kmh) AS avg_wind_peakgust_w
+	,max(wind_peakgust_kmh) AS max_wind_peakgust_w
 	,round(avg(avg_pressure_hpa),2) AS avg_pressure_w
 	,sum(sun_minutes) AS sum_sun_minutes_w
 FROM {{ref('prep_weather_daily')}} pwd
